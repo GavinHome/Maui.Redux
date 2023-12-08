6aeb6dc78baf3bb049ee75356cdc4614a397aaad
YangXiaoMin <yangxiaoming512@yeah.net>
2023-12-08 08:46:55 +0800

    [update] improve code

--- samples/Pages/Todos/Page/Page.cs
+++ samples/Pages/Todos/Page/Page.cs
@@ -1,12 +1,12 @@
 ﻿using DynamicData.Binding;
+using Microsoft.Maui.Controls.Shapes;
 using ReactiveUI;
 using samples.Pages.Todos.Report;
-using samples.Pages.Todos.Todo;
 using System.Collections;
 using System.Collections.ObjectModel;
 using System.Reactive.Subjects;
-
 namespace samples.Pages.Todos.Page;
+
 using Action = Redux.Action;
 
 public partial class ToDoListPage : Page<PageState, Dictionary<string, dynamic>>
@@ -37,7 +37,7 @@ public partial class ToDoListPage : Page<PageState, Dictionary<string, dynamic>>
                      new ScrollView()
                      {
                          Content = new VerticalStackLayout()
-                         {   
+                         {
                              Padding = new Thickness(10,0,10,100),
                              Spacing = 25,
                              Children =
@@ -53,15 +53,11 @@ public partial class ToDoListPage : Page<PageState, Dictionary<string, dynamic>>
             };
             content.Grow(1f);
 
-            var addButton = new AddButton()
-            {
-                ZIndex = 100,
-                OnAddTodo = ReactiveCommand.Create(() => dispatch(new Action("onAdd")))
-            };
+            var addButton = buildAddBtnView(dispatch);
 
             return new ContentPage()
             {
-                Title = "ToDoList",
+                Title = state.Title,
                 Content = new FlexLayout()
                 {
                     Direction = Microsoft.Maui.Layouts.FlexDirection.Column,
@@ -77,12 +73,56 @@ public partial class ToDoListPage : Page<PageState, Dictionary<string, dynamic>>
     {
     }
 
-    private static PageState initState(Dictionary<string, dynamic>? param) => new() { ToDos = [] };
+    private static PageState initState(Dictionary<string, dynamic>? param) => new()
+    {
+        ToDos = [],
+        Title = param?.GetValueOrDefault("title") ?? "ToDoList"
+    };
+
+    private static Grid buildAddBtnView(Dispatch dispatch)
+    {
+        return new Grid()
+        {
+            ZIndex = 100,
+            Children =
+            {
+                new Border()
+                {
+                    HorizontalOptions = new LayoutOptions(LayoutAlignment.End, true),
+                    VerticalOptions = new LayoutOptions(LayoutAlignment.End, true),
+                    HeightRequest = 50,
+                    WidthRequest = 50,
+                    Padding = new Thickness(8,5,12,8),
+                    Background = new SolidColorBrush(Color.Parse("#bbe9d4ff")),
+                    StrokeThickness = 0,
+                    Margin = new Thickness(0,0,15,-35),
+                    StrokeShape = new RoundRectangle()
+                    {
+                        CornerRadius = 15,
+                    },
+                    Content = new Microsoft.Maui.Controls.Shapes.Path()
+                    {
+                        HorizontalOptions= LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.Center,
+                        Fill = new SolidColorBrush(Colors.Black),
+                        Data = new PathGeometryConverter().ConvertFromString("M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z") as PathGeometry,
+                    },
+                    GestureRecognizers =
+                    {
+                        new TapGestureRecognizer()
+                        {
+                            Command = ReactiveCommand.Create(() => dispatch(new Action("onAdd")))
+                        }
+                    }
+                }
+            }
+        };
+    }
 
     private static CollectionView buildItemsView(ObservableCollection<Todo.ToDoState> obs, ComponentContext<PageState> ctx)
     {
         var source = new Subject<List<Widget>>();
-        var items = new CollectionView(); 
+        var items = new CollectionView();
         items.BindingContext = source.BindTo<IEnumerable, CollectionView, IEnumerable>(items, items => items.ItemsSource);
         _ = obs.ToObservableChangeSet().Subscribe(x =>
         {
--- samples/Pages/Todos/Page/State.cs
+++ samples/Pages/Todos/Page/State.cs
@@ -9,6 +9,8 @@ namespace samples.Pages.Todos.Page;
 public class PageState : ReactiveObject
 {
     [Reactive] public ObservableCollection<ToDoState>? ToDos { get; init; }
+
+    [Reactive] public string Title { get; init; } = "TodoList";
     
     public override string ToString()
     {
